30332b69d113a79bf2bb1176f2415644f4dd1b16
royal_T_baby <mashabodovskaya@gmail.com>
2023-06-23 13:04:13 +0200

    My first game

---
+++ game.py
@@ -0,0 +1,81 @@
+import random
+from tkinter import *
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = '#33D745'
+FOOD_COLOR = '#F9F5F5'
+BACKGROUND_COLOR = '#0b2154'
+
+
+class Snake:
+
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        
+
+
+class Food:
+
+    def __init__(self):
+
+        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1)*SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE)-1)*SPACE_SIZE
+
+        self.coordonates = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
+
+def next_turn():
+    pass
+
+
+def change_direction(new_direction):
+    pass
+
+
+def check_collisions():
+    pass
+
+
+def game_over():
+    pass
+
+
+window = Tk()
+
+window.title("Snake game")
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score: {}".format(score), font=('consolas', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
+
+window.mainloop()
